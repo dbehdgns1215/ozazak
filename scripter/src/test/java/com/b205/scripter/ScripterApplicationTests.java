9e362142422a38dab2f4baffea3e67da6bdfcaaf
dbehdgns1215 <dbehdgns1215@naver.com>
2026-01-21 10:00:55 +0900

    chore: init backend project

---
+++ scripter/src/test/java/com/b205/scripter/ScripterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b205.scripter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScripterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
