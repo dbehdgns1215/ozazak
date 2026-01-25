dd4ad797705b708b6fe218cff3146e926eb0d5e0
dbehdgns1215 <dbehdgns1215@naver.com>
2026-01-26 01:02:00 +0900

    feat: extract request and response class

--- back/presentation/src/main/java/com/b205/ozazak/presentation/auth/SignupController.java
+++ back/presentation/src/main/java/com/b205/ozazak/presentation/auth/SignupController.java
@@ -1,15 +1,12 @@
 package com.b205.ozazak.presentation.auth;
 
 import com.b205.ozazak.application.auth.port.in.SignupUseCase;
+import com.b205.ozazak.presentation.auth.dto.SignupRequest;
+import com.b205.ozazak.presentation.auth.dto.SignupResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,27 +32,5 @@ public class SignupController {
                 .build());
         
         return ResponseEntity.ok(new SignupResponse(jwt));
-    }
-
-    @Getter
-    @Setter
-    public static class SignupRequest {
-        @NotBlank @Email
-        private String email;
-
-        @NotBlank @Size(min = 8)
-        private String password;
-
-        @NotBlank @Size(min = 2)
-        private String name;
-
-        @NotBlank
-        private String verificationToken;
-    }
-
-    @Getter
-    @RequiredArgsConstructor
-    public static class SignupResponse {
-        private final String accessToken;
     }
 }
