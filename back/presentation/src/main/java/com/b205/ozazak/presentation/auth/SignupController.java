ee0724aeb566ab4e4aad42a56db841355244aa3b
dbehdgns1215 <dbehdgns1215@naver.com>
2026-01-26 14:37:23 +0900

    feat: signup email verification

--- back/presentation/src/main/java/com/b205/ozazak/presentation/auth/SignupController.java
+++ back/presentation/src/main/java/com/b205/ozazak/presentation/auth/SignupController.java
@@ -1,6 +1,10 @@
 package com.b205.ozazak.presentation.auth;
 
+import com.b205.ozazak.application.auth.command.SignupCommand;
 import com.b205.ozazak.application.auth.port.in.SignupUseCase;
+import com.b205.ozazak.domain.account.vo.AccountName;
+import com.b205.ozazak.domain.account.vo.Email;
+import com.b205.ozazak.domain.account.vo.Password;
 import com.b205.ozazak.presentation.auth.dto.SignupRequest;
 import com.b205.ozazak.presentation.auth.dto.SignupResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -24,10 +28,10 @@ public class SignupController {
     @Operation(summary = "Registration with email verification token")
     @PostMapping("/signup")
     public ResponseEntity<SignupResponse> signup(@Valid @RequestBody SignupRequest request) {
-        String jwt = signupUseCase.signup(SignupUseCase.SignupCommand.builder()
-                .email(request.getEmail())
-                .password(request.getPassword())
-                .name(request.getName())
+        String jwt = signupUseCase.signup(SignupCommand.builder()
+                .email(new Email(request.getEmail()))
+                .password(new Password(request.getPassword()))
+                .name(new AccountName(request.getName()))
                 .verificationToken(request.getVerificationToken())
                 .build());
         
