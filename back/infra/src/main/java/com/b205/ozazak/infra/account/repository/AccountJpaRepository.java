2c56d9e0c03f92dd93eacf23990ea78a4f3d0d5c
dbehdgns1215 <dbehdgns1215@naver.com>
2026-01-27 16:27:20 +0900

    feat: add streak admin REST API

--- back/infra/src/main/java/com/b205/ozazak/infra/account/repository/AccountJpaRepository.java
+++ back/infra/src/main/java/com/b205/ozazak/infra/account/repository/AccountJpaRepository.java
@@ -3,7 +3,11 @@ package com.b205.ozazak.infra.account.repository;
 import com.b205.ozazak.infra.account.entity.AccountJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountJpaRepository extends JpaRepository<AccountJpaEntity, Long> {
     boolean existsByEmail(String email);
-    java.util.Optional<AccountJpaEntity> findByEmail(String email);
+    Optional<AccountJpaEntity> findByEmail(String email);
+
+    Optional<AccountJpaEntity> findById(Long accountId);
 }
