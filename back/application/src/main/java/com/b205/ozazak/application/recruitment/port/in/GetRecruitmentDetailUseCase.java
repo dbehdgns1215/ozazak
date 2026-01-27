9f6a16c46c39b2420f4f2661a2653bb763e1b1cb
Y-eseong <yodeng94@naver.com>
2026-01-27 16:52:52 +0900

    feat: add recruitments list

--- back/application/src/main/java/com/b205/ozazak/application/recruitment/port/in/GetRecruitmentDetailUseCase.java
+++
@@ -1,7 +0,0 @@
-package com.b205.ozazak.application.recruitment.port.in;
-
-import com.b205.ozazak.application.recruitment.result.RecruitmentDetailResult;
-
-public interface GetRecruitmentDetailUseCase {
-    RecruitmentDetailResult getRecruitmentDetail(Long recruitmentId);
-}
---
+++ back/application/src/main/java/com/b205/ozazak/application/recruitment/port/out/LoadRecruitmentListPort.java
@@ -0,0 +1,10 @@
+package com.b205.ozazak.application.recruitment.port.out;
+
+import com.b205.ozazak.domain.recruitment.entity.Recruitment;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface LoadRecruitmentListPort {
+    List<Recruitment> loadRecruitmentList(LocalDate from, LocalDate to);
+}
