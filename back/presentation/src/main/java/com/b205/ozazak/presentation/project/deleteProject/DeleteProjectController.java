c41c4e78dd6311523d167172f4dbbe4aaa6d4be6
Y-eseong <yodeng94@naver.com>
2026-01-27 13:45:43 +0900

    fix: restructure project

---
+++ back/presentation/src/main/java/com/b205/ozazak/presentation/project/deleteProject/DeleteProjectController.java
@@ -0,0 +1,30 @@
+package com.b205.ozazak.presentation.project.deleteProject;
+
+import com.b205.ozazak.application.project.port.in.DeleteProjectUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/projects")
+@RequiredArgsConstructor
+@Tag(name = "Project", description = "Project API")
+public class DeleteProjectController {
+
+    private final DeleteProjectUseCase deleteProjectUseCase;
+
+    @Operation(summary = "Delete Project")
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<Void> deleteProject(
+            @AuthenticationPrincipal Long userId,
+            @PathVariable Long projectId) {
+        deleteProjectUseCase.deleteProject(userId, projectId);
+        return ResponseEntity.noContent().build();
+    }
+}
--- back/presentation/src/main/java/com/b205/ozazak/presentation/project/getProjectList/GetProjectListController.java
+++ back/presentation/src/main/java/com/b205/ozazak/presentation/project/getProjectList/GetProjectListController.java
@@ -1,8 +1,7 @@
-package com.b205.ozazak.presentation.project.controller;
+package com.b205.ozazak.presentation.project.getProjectList;
 
 import com.b205.ozazak.application.project.port.in.GetProjectUseCase;
 import com.b205.ozazak.application.project.result.GetProjectResult;
-import com.b205.ozazak.presentation.project.dto.response.ProjectResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -11,7 +10,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,22 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/projects")
 @RequiredArgsConstructor
 @Tag(name = "Project", description = "Project API")
-public class ProjectReadController {
+public class GetProjectListController {
 
     private final GetProjectUseCase getProjectUseCase;
 
-    @Operation(summary = "Get Project Detail")
-    @GetMapping("/{projectId}")
-    public ResponseEntity<ProjectResponse> getProject(@AuthenticationPrincipal Long userId, @PathVariable Long projectId) {
-        GetProjectResult result = getProjectUseCase.getProject(userId, projectId);
-        return ResponseEntity.ok(ProjectResponse.from(result));
-    }
-
     @Operation(summary = "Get Project List")
     @GetMapping
-    public ResponseEntity<Page<ProjectResponse>> getProjectList(@AuthenticationPrincipal Long userId,
+    public ResponseEntity<Page<GetProjectListResponse>> getProjectList(
+            @AuthenticationPrincipal Long userId,
             Pageable pageable) {
         Page<GetProjectResult> results = getProjectUseCase.getProjectList(userId, pageable);
-        return ResponseEntity.ok(results.map(ProjectResponse::from));
+        return ResponseEntity.ok(results.map(GetProjectListResponse::from));
     }
 }
