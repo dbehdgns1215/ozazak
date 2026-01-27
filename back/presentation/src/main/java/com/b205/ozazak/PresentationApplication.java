a5126e685c5938351a40a3eddbc45206f9ad7dac
dbehdgns1215 <dbehdgns1215@naver.com>
2026-01-27 16:26:41 +0900

    feat: Implement streak batch calculation

--- back/presentation/src/main/java/com/b205/ozazak/PresentationApplication.java
+++ back/presentation/src/main/java/com/b205/ozazak/PresentationApplication.java
@@ -2,10 +2,12 @@ package com.b205.ozazak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import io.github.cdimascio.dotenv.Dotenv;
 import java.io.File;
 
 @SpringBootApplication
+@EnableScheduling
 public class PresentationApplication {
     public static void main(String[] args) {
 
